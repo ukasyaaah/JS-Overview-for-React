104a3e87202ba0b94840a43a3f04d94da05eee13
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-08-10 18:39:26 +0700

    first class function

---
+++ function.js
@@ -0,0 +1,33 @@
+/// First class function
+
+//1. disimpan dalam variable -> funct expression
+let hello = () => console.log(`Hello`);
+hello();
+
+//2. Dikirim sbg argument
+function nama(nama, filter) {
+  console.log(filter(nama));
+}
+
+function toUpper(nama) {
+  return nama.toUpperCase();
+}
+nama("Hani", toUpper);
+
+//3. sbg return value
+function kali(x) {
+  return (y) => console.log(x * y);
+}
+
+let kali2 = kali(2);
+kali2(4);
+
+
+//4. Object yg memiliki property
+function contoh() {
+  contoh.properti = "Properti Funct";
+  contoh.jurusan = "Teknik Informatika";
+}
+contoh();
+console.log(contoh);
+console.log(contoh.properti);
